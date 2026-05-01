55d46c50726bf122487d9dedad148d0e4f1e29c4
Raja Hafify <rajahafify.zaed@gmail.com>
2026-05-02 02:56:26 +0800

    feat: add Orbwalker page with Lenis-based parallax

--- app/javascript/controllers/parallax_controller.js
+++ app/javascript/controllers/parallax_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import Lenis from "@studio-freight/lenis"
 
 export default class extends Controller {
   static targets = ["layer"]
@@ -7,58 +8,34 @@ export default class extends Controller {
   }
 
   connect() {
-    this._pending = false
-    this._rafId = null
-
     this.layerTargets.forEach((layer) => {
       layer.style.willChange = "transform"
     })
 
-    this._onLenisScroll = (data) => {
-      this._scrollY = data.scroll
-      if (!this._pending) {
-        this._pending = true
-        this._rafId = requestAnimationFrame(() => {
-          this._pending = false
-          this._rafId = null
-          this._applyTransforms()
-        })
-      }
-    }
-
-    this._readyHandler = (e) => {
-      this._lenis = e.detail
-      this._lenis.on("scroll", this._onLenisScroll)
-      this._scrollY = this._lenis.scroll || 0
-      this._applyTransforms()
-    }
-
-    if (window.__lenis) {
-      this._lenis = window.__lenis
-      this._lenis.on("scroll", this._onLenisScroll)
-      this._scrollY = this._lenis.scroll || 0
-      this._applyTransforms()
-    } else {
-      window.addEventListener("lenis:ready", this._readyHandler)
-    }
-  }
-
-  _applyTransforms() {
-    const y = this._scrollY || 0
-    this.layerTargets.forEach((layer) => {
-      const speed = parseFloat(layer.dataset.parallaxSpeed) || this.speedValue
-      layer.style.transform = `translate3d(0, ${y * speed}px, 0)`
+    this.lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true
     })
+
+    this.lenis.on("scroll", ({ scroll }) => {
+      this.layerTargets.forEach((layer) => {
+        const speed = parseFloat(layer.dataset.parallaxSpeed) || this.speedValue
+        layer.style.transform = `translate3d(0, ${scroll * speed}px, 0)`
+      })
+    })
+
+    const raf = (time) => {
+      this.lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
   }
 
   disconnect() {
-    window.removeEventListener("lenis:ready", this._readyHandler)
-    if (this._lenis) {
-      this._lenis.off("scroll", this._onLenisScroll)
-      this._lenis = null
-    }
-    if (this._rafId) {
-      cancelAnimationFrame(this._rafId)
+    if (this.lenis) {
+      this.lenis.destroy()
+      this.lenis = null
     }
   }
 }
